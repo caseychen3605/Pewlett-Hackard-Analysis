2295c81414497fe6da9261cbe1cc3d5363d2f5ba
unknown <caseychen3605@gmail.com>
2020-12-10 12:16:43 -0800

    Adding additional_list_queries.sql

---
+++ Queries/additional_list_queries.sql
@@ -0,0 +1,31 @@
+---- CREATE ADDITIONAL LISTS ----
+-- Creating employee information
+-- Checking the salaries table
+SELECT * FROM salaries
+ORDER BY to_date DESC;
+
+SELECT emp_no, first_name, last_name, gender
+INTO emp_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+	AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+-- Joining employees and salaries
+SELECT e.emp_no, 
+	e.first_name, 
+	e.last_name, 
+	e.gender, 
+	s.salary, 
+	de.to_date
+INTO emp_info
+FROM employees as e
+INNER JOIN salaries as s
+ON (e.emp_no = s.emp_no)
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+--  Joining a third table
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+	AND (e.hire_date BETWEEN '1985-01-01' AND '1988-12-31')
+-- Adding birth and hire date filters
+	AND (de.to_date = '9999-01-01')
+-- Adding to_date filter for current employees
