65c1bb55d271573696245b5544a97dca624f6a5a
Casey Chen <70241923+caseychen3605@users.noreply.github.com>
2021-02-15 21:28:29 -0800

    Update count_groupby_order_query

--- Queries/count_groupby_order_queries.sql
+++ Queries/count_groupby_order_queries.sql
@@ -6,6 +6,6 @@ ON ce.emp_no = de.emp_no
 GROUP BY de.dept_no
 ORDER BY de.dept_no;
 -- 'SELECT COUNT' gives us the total number of employees
--- 'LEFT JOIN' was used because we wante all employee numbers from Table 1 to be included
+-- 'LEFT JOIN' was used because we wanted all employee numbers from Table 1 to be included
 -- 'GROUP BY' gives us the number of employees retiring from each department
 -- 'ORDER BY' puts the output data in order
